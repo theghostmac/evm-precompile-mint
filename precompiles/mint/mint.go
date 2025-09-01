43757f87ce01c83e706c2027e75fa5339b9319a3
MacBobby Chibuzor <theghostmac@gmail.com>
2025-09-01 13:46:14 +0100

    [feat] add the constructor, adding missing methods now

--- precompiles/mint/mint.go
+++ precompiles/mint/mint.go
@@ -1,4 +1,42 @@
 package mint
 
-type Precompile struct {
+import (
+	"embed"
+
+	storetypes "cosmossdk.io/store/types"
+	cmn "github.com/cosmos/evm/precompiles/common"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/vm"
+)
+
+const (
+	abiPath = "abi.json"
+	GasMint = 25_000
+)
+
+var f embed.FS
+
+var _ vm.PrecompiledContract = &Precompile{}
+
+// NewPRecompile creates a new Mint Precompile instance as a PrecompiledContract interface.
+func NewPrecompile(authority string, bankKeeper cmn.BankKeeper) (*Precompile, error) {
+	newABI, err := cmn.LoadABI(f, abiPath)
+	if err != nil {
+		return nil, err
+	}
+
+	p := &Precompile{
+		Precompile: cmn.Precompile{
+			ABI:                  newABI,
+			KvGasConfig:          storetypes.GasConfig{},
+			TransientKVGasConfig: storetypes.GasConfig{},
+		},
+		authority:  authority,
+		bankKeeper: bankKeeper,
+	}
+
+	// Set the address of the Mint precompile contract. would use 0x1111 for easy testing
+	p.SetAddress(common.HexToAddress("0x0000000000000000000000000000000000001111"))
+
+	return p, nil
 }
